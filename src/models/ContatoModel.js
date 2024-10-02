60053fc5292a65310342810fd978f2b04bc43fa1
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-02 12:27:30 -0400

    chore: created edit contact route

--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -57,4 +57,13 @@ Contato.buscarPorId = async function (id) {
   return contato;
 };
 
+Contato.prototype.edit = async function (id) {
+  if (typeof id !== "string") return;
+  this.valida();
+  if (this.errors.length > 0) return;
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, {
+    new: true,
+  });
+};
+
 module.exports = Contato;
