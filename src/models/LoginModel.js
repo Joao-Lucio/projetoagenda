d92eb27264719e26051222ff2278de4a27ae60ab
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-02 00:08:31 -0400

    chore: created function login

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -20,14 +20,29 @@ class Login {
     this.valida();
     if (this.errors.length > 0) return;
 
-    try {
-      await this.userExists();
-      if (this.errors.length > 0) return;
-      const salt = bcryptjs.genSaltSync();
-      this.body.password = bcryptjs.hashSync(this.body.password, salt);
-      this.user = await LoginModel.create(this.body);
-    } catch (e) {
-      console.log(e);
+    await this.userExists();
+    if (this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
+    this.user = await LoginModel.create(this.body);
+  }
+
+  async login() {
+    this.valida();
+    if (this.errors.length > 0) return;
+
+    this.user = await LoginModel.findOne({ email: this.body.email });
+
+    if (!this.user) {
+      this.errors.push("Usuário não existe.");
+      return;
+    }
+
+    if (!bcryptjs.compareSync(this.body.password, this.user.password)) {
+      this.errors.push("Senha inválida");
+      this.user = null;
+      return;
     }
   }
 
@@ -55,8 +70,8 @@ class Login {
   }
 
   async userExists() {
-    const user = await LoginModel.findOne({ email: this.body.email });
-    if (user) this.errors.push("Usuário já existe.");
+    this.user = await LoginModel.findOne({ email: this.body.email });
+    if (this.user) this.errors.push("Usuário já existe.");
   }
 }
 
