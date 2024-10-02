6653430c2c943f47749c1b6193720cbd3f70b711
JOAO-LUCIO <joao.silvarrr@gmail.com>
2024-10-02 12:47:59 -0400

    chore: created delete route

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -29,6 +29,7 @@ exports.editIndex = async function (req, res) {
   const contato = await Contato.buscarPorId(req.params.id);
   if (!contato) return res.render("404");
   res.render("contato", { contato });
+  return;
 };
 
 exports.edit = async (req, res) => {
@@ -45,8 +46,19 @@ exports.edit = async (req, res) => {
     req.session.save(() =>
       res.redirect(`/contato/index/${contato.contato._id}`)
     );
+    return;
   } catch (e) {
     console.log(e);
     return res.render("404");
   }
+};
+
+exports.delete = async (req, res) => {
+  if (!req.params.id) return res.render("404");
+  const contato = await Contato.delete(req.params.id);
+  if (!contato) return res.render("404");
+
+  req.flash("success", "Contato apagado com sucesso.");
+  req.session.save(() => res.redirect(`/`));
+  return;
 };
